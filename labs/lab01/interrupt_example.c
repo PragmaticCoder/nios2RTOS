c0284e3afefbcfddb438c709fd05d97a1c6d9719
Alvi Akbar <ala273@mail.usask.ca>
2020-02-04 02:15:17 -0600

    temporary reduced time

--- labs/lab01/interrupt_example.c
+++ labs/lab01/interrupt_example.c
@@ -48,9 +48,8 @@ int main(void)
     questions_count = 0;
     hex_count = 30;
 
-
     /* set the interval timer period for scrolling the LED lights */
-    int counter = 50000000; // 1/(50 MHz) x (50000000) = 1sec
+    int counter = 50000000/4; // 1/(50 MHz) x (50000000) = 1sec
 
     *(interval_timer_ptr + 0x2) = (counter & 0xFFFF);
     *(interval_timer_ptr + 0x3) = (counter >> 16) & 0xFFFF;
