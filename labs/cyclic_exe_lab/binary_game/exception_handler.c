ca1cff990a315327ad55a0dc0780cbcaf41e83dd
Alvi Akbar <alvi.akbar@outlook.com>
2020-02-06 14:25:58 -0600

    initial trial project setup

--- labs/cyclic_exe_lab/binary_game/exception_handler.c
+++ labs/cyclic_exe_lab/binary_game/exception_handler.c
@@ -129,17 +129,14 @@ void the_exception(void)
 void interrupt_handler(void) {
     int ipending;
     NIOS2_READ_IPENDING(ipending);
-    /* Push button has more priority */
-    if (ipending & 0x2) // pushbuttons are interrupt level 0
-    {
-        pushbutton_ISR();
-    }
-
-    if (ipending & 0x1) // interval timer is interrupt level 1
+    if (ipending & 0x1) // interval timer is interrupt level 0
     {
         interval_timer_ISR();
     }
-
+    if (ipending & 0x2) // pushbuttons are interrupt level 1
+    {
+        pushbutton_ISR();
+    }
     // else, ignore the interrupt
     return;
 }
