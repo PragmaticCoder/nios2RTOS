c5b12c39fc15523ac5d75ea042b76e77f2d3c05a
Alvi Akbar <alvi.akbar@outlook.com>
2020-02-27 15:39:50 -0600

    brought in the files from lab2 linux

--- labs/lab_02/Micrium/DE2-115_Computer/Verilog/software/ucos2nios2/globals.h
+++ labs/lab_02/Micrium/DE2-115_Computer/Verilog/software/ucos2nios2/globals.h
@@ -34,11 +34,15 @@
 
 /* Definition of Semaphores */
 OS_EVENT *SEM_read_PS2;
+OS_EVENT *SEM_read_PS2_done;
 OS_EVENT *SEM_read_KEYS;
 OS_EVENT *SEM_state_change;
 OS_EVENT *SEM_timer_start;
 OS_EVENT *SEM_flash_success;
 OS_EVENT *SEM_flash_fail;
+OS_EVENT *SEM_add_code;
+OS_EVENT *SEM_delete_code;
+
 /* States */
 typedef enum
 {
