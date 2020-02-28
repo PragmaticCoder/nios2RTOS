3b3b5549fefaeb62a2b4bb5d1dc3e3e41dc68fe0
Alvi Akbar <ala273@mail.usask.ca>
2020-02-28 00:34:41 -0600

    minor fixes to my lab

--- labs/lab_02_linux/Micrium/DE2-115_Computer/Verilog/software/ucos2nios2/hello_ucosii.c
+++ labs/lab_02_linux/Micrium/DE2-115_Computer/Verilog/software/ucos2nios2/hello_ucosii.c
@@ -177,7 +177,7 @@ void Task_read_PS2(void *pdata)
 
     if ((state == CODE || state == PROG) && PS2_num != -1)
     {
-      if (cur_input_idx % 2 == 0)
+      if (cur_input_idx % 2 == 1)
         cur_input_code[cur_input_idx++] = timer_code;
 
       cur_input_code[cur_input_idx++] = PS2_num;
