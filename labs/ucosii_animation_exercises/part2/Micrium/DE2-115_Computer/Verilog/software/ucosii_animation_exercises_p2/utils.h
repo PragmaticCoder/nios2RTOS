ba06e29e33d577c78f98fe0846b21e91e7db893f
PragmaticCoder <alvi.akbar@realpythonengineer.com>
2020-03-10 06:17:58 -0600

    minor formatting fix. Also added function prototypes for VGA_animated_color

--- labs/ucosii_animation_exercises/part2/Micrium/DE2-115_Computer/Verilog/software/ucosii_animation_exercises_p2/utils.h
+++ labs/ucosii_animation_exercises/part2/Micrium/DE2-115_Computer/Verilog/software/ucosii_animation_exercises_p2/utils.h
@@ -11,17 +11,26 @@
 /* Function Prototypes */
 void
 Check_KEYs(int*, int*, int*, int*);
+
 void
 VGA_text(int, int, char*);
+
 void
 VGA_box(int, int, int, int, short);
+
 void
 VGA_char_box(int, int, short);
+
 int
 resample_rgb(int, int);
+
 int
 get_data_bits(int);
+
 void
 clear_all_chars();
+
+void
+VGA_animated_char(int x, int y, char* text_ptr, short char_bg_color);
 
 #endif /* UTILS_H_ */
